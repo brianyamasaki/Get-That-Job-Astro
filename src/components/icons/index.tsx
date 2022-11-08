19a112bef50df8657377dd63a7b0ba230e8cf2c4
Brian Yamasaki <brian.yamasaki@gmail.com>
2022-11-07 16:04:14 -0800

    choose a mid gray for navigation carets

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -4,12 +4,14 @@ import type { ReactNode } from 'react';
 type CaretProps = {
   size?: number;
   rotation?: number;
+  color?: string;
   children?: ReactNode;
 }
 
-export const Caret = ({size, rotation}: CaretProps) => {
+export const Caret = ({size, rotation, color}: CaretProps) => {
   const width = size ? `${24 * size}px` : '24px';
   const rotate = rotation ? rotation : 0;
+  const colorStr = color ? color : '#888888';
   const props = {
     width,
     height: width,
@@ -19,7 +21,7 @@ export const Caret = ({size, rotation}: CaretProps) => {
 
   return (
     <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={props}>
-      <path d="M9 17.898C9 18.972 10.2649 19.546 11.0731 18.8388L17.3838 13.3169C18.1806 12.6197 18.1806 11.3801 17.3838 10.6829L11.0731 5.16108C10.2649 4.45388 9 5.02785 9 6.1018V17.898Z" fill="#212121"/>
+      <path d="M9 17.898C9 18.972 10.2649 19.546 11.0731 18.8388L17.3838 13.3169C18.1806 12.6197 18.1806 11.3801 17.3838 10.6829L11.0731 5.16108C10.2649 4.45388 9 5.02785 9 6.1018V17.898Z" fill={colorStr}/>
     </svg>
   )
 }
